4da272671438a5c412bd7ae4dfd8fcdbf16b9bef
avyishi <avyishi@gmail.com>
2015-01-07 22:58:20 -0800

    Fixed cart issue

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,6 +28,7 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id) #@cart.line_items.build(product: product)
+    
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart }
