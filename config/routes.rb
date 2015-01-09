4771c598ce1ee082ef833c890bb5c4ece77528a7
avyishi <avyishi@gmail.com>
2015-01-09 00:02:22 -0800

    Added code to capture order details

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
 
   get 'shops/index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   root 'shops#index', as: 'shops'
   
